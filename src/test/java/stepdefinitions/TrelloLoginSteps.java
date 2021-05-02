86a499ac32c6b8d23f8d100ba337b51dd591e8fb
Ebony Lilly <ebony.lilly25@gmail.com>
2021-05-02 14:00:20 -0400

    implemented negative test steps in TrelloLoginSteps class second commit

--- src/test/java/stepdefinitions/TrelloLoginSteps.java
+++ src/test/java/stepdefinitions/TrelloLoginSteps.java
@@ -51,6 +51,9 @@ public class TrelloLoginSteps {
         WebElement emailInput = Hooks.driver().findElement(loc_email);
         WebElement passwordInput = Hooks.driver().findElement(loc_password);
         WebElement loginBttn = Hooks.driver().findElement(loc_log_bttn);
+        emailInput.sendKeys("nijatmn@gmail.com");
+        passInput.sendKeys("pubg!max");
+        WebElement loginBttn = Hooks.driver().findElement(loc_log_bttn);
         loginBttn.click();
     }
 
@@ -66,10 +69,28 @@ public class TrelloLoginSteps {
 
     @When("^User logins with invalid credentials$")
     public void user_logins_with_invalid_credentials() {
+        By loc_email = By.id("user");
+        By loc_pass = By.id("password");
+        By loc_log_bttn = By.id("login");
+        WebElement emailInput = Hooks.driver().findElement(loc_email);
+        WebElement passInput = Hooks.driver().findElement(loc_pass);
+        Faker faker = new Faker();
+        String fakeEmail = faker.internet().emailAddress();
+        String fakePass = faker.internet().password();
+        emailInput.sendKeys(fakeEmail);
+        passInput.sendKeys(fakePass);
+        WebElement loginBttn = Hooks.driver().findElement(loc_log_bttn);
+        loginBttn.click(); 
+
     }
 
     @Then("^User should be able to see error message$")
     public void user_should_be_able_to_see_error_message(){
+        By loc_error_msg = By.cssSelector("div#error >p");
+        WebElement errorMsgElement = Hooks.driver().findElement(loc_error_msg);
+        String actualErrorMessage = errorMsgElement.getText();
+        Assert.assertEquals("There isn't an account for this username",actualErrorMessage);
     }
+
 
 }
